72c5ebcbdea8e4b5d542611aee177154bee532c7
Nirma J Mali <nirmamali8618@gmail.com>
2026-04-03 01:41:53 +0530

    backend basic done

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,35 @@
-import { Express } from "express";
+import express from "express";
+import { createServer } from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+
+const start = async () => {
+    app.set("mongo_user")
+    const connectionDb = await mongoose.connect("mongodb+srv://nirmamali9845_db_user:yeb1khREEJ817sQ8@cluster0.zjnwv2o.mongodb.net/?appName=Cluster0")
+
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENING ON PORT 8000")
+    });
+
+}
+
+start();
