9b610617667151ce21722aaecf5bc74afc478f8d
Nirma J Mali <nirmamali8618@gmail.com>
2026-04-17 00:15:04 +0530

    user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 
+// Below schema is used to define the structure of the user document in the MongoDB database. It includes fields for name, username, password and token. The username field is unique and required, while the other fields are also required.
 const userScheme = new Schema(
     {
         name: { type: String, required: true },
